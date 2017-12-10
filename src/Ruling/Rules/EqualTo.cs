43daf350cf10e375e5643aaf3cb327424f353a35
tdeschryver <timdeschryver@outlook.com>
2017-12-10 20:40:13 +0100

    Add rule: EqualTo (#9)

---
+++ src/Ruling/Rules/EqualTo.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Ruling
+{
+    public static partial class Rule
+    {
+        public const string EqualToMessage = "must be equal to {0}";
+
+        public static Func<TObject, (bool valid, string key, string message)> EqualTo<TObject, TEqual>(Expression<Func<TObject, IEquatable<TEqual>>> selector, IEquatable<TEqual> other, string message = null, string key = null)
+        {
+            var ruleKey = GetKey(selector, key);
+            var ruleMessage = other == null
+                ? string.Format(EqualToMessage, "null")
+                : GetMessage(message, string.Format(EqualToMessage, other));
+
+            return (TObject @object) =>
+            {
+                var value = selector.Compile().Invoke(@object);
+                if (IsInvalidEqualTo(value, other))
+                {
+                    return (false, ruleKey, ruleMessage);
+                }
+
+                return (true, ruleKey, ruleMessage);
+            };
+        }
+
+        public static Func<TObject, (bool valid, string key, string message)> EqualTo<TObject, TEqual>(Expression<Func<TObject, IEquatable<TEqual>>> selector, Func<TObject, IEquatable<TEqual>> other, string message = null, string key = null)
+        {
+            var ruleKey = GetKey(selector, key);
+
+            return (TObject @object) =>
+            {
+                var value = selector.Compile().Invoke(@object);
+                var otherValue = other?.Invoke(@object);
+                var ruleMessage = otherValue == null
+                    ? string.Format(EqualToMessage, "null")
+                    : GetMessage(message, string.Format(EqualToMessage, otherValue));
+
+                if (IsInvalidEqualTo(value, otherValue))
+                {
+                    return (false, ruleKey, ruleMessage);
+                }
+
+                return (true, ruleKey, ruleMessage);
+            };
+        }
+
+        static bool IsInvalidEqualTo<TEqual>(IEquatable<TEqual> value, IEquatable<TEqual> other)
+            => value == null || other == null
+                ? true
+                : !value.Equals(other);
+    }
+}
